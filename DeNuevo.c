9c54618ceef58db19d5ee1c20915ed2c9f05bf65
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-11-01 12:44:14 -0500

    Se agrego case 4 y 5

--- DeNuevo.c
+++ DeNuevo.c
@@ -22,13 +22,42 @@ bool esPrimo(int num)
     }
 }
 
+int cantidadDigitos(int num)
+{
+    int cantidad = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        cantidad++;
+    }
+    return cantidad;
+}
+
+int digitoMayor(int num)
+{
+    int Mayor = -num;
+    int auxiliar;
+    while (num != 0)
+    {
+        auxiliar = num % 10;
+        if (auxiliar > Mayor)
+        {
+            Mayor = auxiliar;
+        }
+        num = num/10;
+    }
+    return Mayor;
+}
+
 int main()
 {
     int opcion = 0;
-    int num1 = 0, num2 = 0;
+    int num1 = 0, num2 = 0, num4 = 0, num5 = 0;
     printf("Ingrese el valor del ejercicio que desea realizar: \n"
            "1. Verificar si un numero es primo.\n"
-           "2. Numero de primos que hay hasta un numero.\n");
+           "2. Numero de primos que hay hasta un numero.\n"
+           "3. Primer numero primo antes de 32768.\n"
+           "4. Cantidad de Digitos\n");
     scanf("%d", &opcion);
     switch (opcion)
     {
@@ -74,7 +103,15 @@ int main()
                 break;
             }
         }
+    case 4:
+        printf("Cantidad de digitos del numero. Ingrese su numero: \n");
+        scanf("%d", &num4);
+        printf("La cantidad de digitos de %d es: %d.\n", num4, cantidadDigitos(num4));
 
+    case 5:
+        printf("Ingrese el numero y se obtendra el digito mayor de dicho numero:\n");
+        scanf("%d", &num5);
+        printf("El numero mayor de %d es %d.\n", num5, digitoMayor(num5));
     default:
         break;
     }
